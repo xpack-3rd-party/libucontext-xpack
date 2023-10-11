e2b8151800655576652e40b0f725d57e21e3bbc6
Liviu Ionescu <ilg@livius.net>
2023-10-11 16:29:31 +0300

    silence warnings for arm

--- arch/arm/makecontext.c
+++ arch/arm/makecontext.c
@@ -20,6 +20,10 @@
 #include "defs.h"
 #include <libucontext/libucontext.h>
 
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#if defined(__clang__)
+#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
+#endif
 
 extern void libucontext_trampoline(void);
 
